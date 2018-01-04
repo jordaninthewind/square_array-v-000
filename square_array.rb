e71ba880d31dc6e1e058518f4f1f9fb1654a9226
Jordan Kline <jordan.kline@gmail.com>
2018-01-04 19:46:32 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,5 @@
-# def square_array(array)
-#   return_array = []
-#   array.each { |el| return_array << el * el }
-#   return_array
-# end
-
 def square_array(array)
-  array.each { |el| el = el * el }
+  return_array = []
+  array.each { |el| return_array << el * el }
+  return_array
 end
